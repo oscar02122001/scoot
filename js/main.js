bfdc3906edd492aabf0acfa17229054e225ab084
oscar02122001 <olimjonismatov01@gmail.com>
2022-06-21 16:46:16 +0500

    to home advice

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 const burger = document.querySelector(".header__burger");
-const nav = document.querySelector(".nav--header");
+const nav = document.querySelector(".header__nav");
+const body = document.querySelector("body");
 
 burger.addEventListener("click", function () {
     burger.classList.toggle("active");
+    body.classList.toggle("freez");
     if (!nav.classList.contains("active")) {
         nav.classList.add("active");
     } else if (!nav.classList.contains("closed")) {
@@ -19,10 +21,29 @@ nav.addEventListener("animationend", function (evt) {
 
 
 const faqs__qst = document.querySelector(".faqs__qst");
-const faqs__disc = document.querySelector(".faqs__desc");
+const faqs__disc = document.querySelector(".faqs__disc");
 
 
-faqs__qst.addEventListener("click", () => {
-    faqs__qst.classList.toggle("opened");
-    faqs__disc.classList.toggle("opened");
+// faqs__qst.addEventListener("click", () => {
+//     faqs__qst.classList.toggle("opened");
+//     faqs__disc.classList.toggle("opened");
+// })
+faqs__qst.addEventListener("click", function () {
+    if (!faqs__qst.classList.contains("opened")) {
+        faqs__qst.classList.add("opened");
+        faqs__disc.classList.add("opened");
+    } else if (!faqs__qst.classList.contains("closing")) {
+        faqs__qst.classList.add("closing");
+        faqs__disc.classList.add("closing");
+    }
+
+})
+
+faqs__disc.addEventListener("animationend", function (evt) {
+    if (evt.animationName === "closing") {
+        faqs__qst.classList.remove("opened");
+        faqs__disc.classList.remove("opened");
+        faqs__qst.classList.remove("closing");
+        faqs__disc.classList.remove("closing")
+    }
 })
